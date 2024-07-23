78322abc22a9e4156c193c4e3751a447cf6182d1
misbat <misbat.dev@gmail.com>
2024-07-23 19:30:10 +0100

    partial stub to do a texture mesh window

---
+++ Canvas.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace TextureMeshEditor
+{
+    public class Canvas
+    {
+        private Texture2D _texture;
+        private Vector2 _size;
+        private Vector2 _zoom = Vector2.one;
+        private Vector2 _offset = Vector2.zero;
+
+        public Canvas(Texture2D texture, Vector2 size)
+        {
+            _texture = texture;
+            _size = size;
+        }
+    }
+}
---
+++ TextureUtility.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor.Graphs;
+using UnityEngine;
+
+namespace TextureMeshEditor
+{
+    public static class TextureUtility
+    {
+        public static Texture2D CreateSinglePixel(Color color)
+        {
+            var pix = new Color[1];
+            pix[0] = color;
+            var result = new Texture2D(1, 1);
+            result.SetPixels(pix);
+            result.Apply();
+            return result;
+        }
+    }
+}
