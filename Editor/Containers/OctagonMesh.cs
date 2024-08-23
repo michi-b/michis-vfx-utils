19ff0befa98984b641be36d51b17a7123d7147c7
misbat <misbat.dev@gmail.com>
2024-08-23 22:54:51 +0200

    extract simple octagon creation in dedicated subclass

--- Editor/Containers/OctagonMesh.cs
+++ Editor/Containers/OctagonMesh.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityEngine.Rendering;
 
 namespace MichisMeshMakers.Editor.Containers
 {
@@ -8,11 +7,8 @@ namespace MichisMeshMakers.Editor.Containers
         public const string AxisLengthFieldName = "_axisLength";
         public const string DiagonalLengthFieldName = "_diagonalLength";
 
-        private const int SimpleOctagonVertexCount = 8;
-        private const int SimpleOctagonIndexCount = 6 * 3;
-
-        private static readonly Vector3[] Vertices = new Vector3[SimpleOctagonVertexCount]; // 8 corners for an octagon
-        private static readonly int[] Indices = new int[SimpleOctagonIndexCount]; // 6 faces with 3 vertices each
+        private static readonly Vector3[] Vertices = new Vector3[Simple.VertexCount];
+        private static readonly int[] Indices = new int[Simple.IndexCount];
 
         [SerializeField] [Range(0f, 1f)] private float _axisLength = 1f;
 
@@ -24,70 +20,103 @@ namespace MichisMeshMakers.Editor.Containers
             RegenerateMesh();
         }
 
-        private void SetVertex(int index, float x, float y)
+        private static void SetVertex(int index, float x, float y)
         {
             Vertices[index] = new Vector3(x, y, 0f);
         }
 
         public void RegenerateMesh()
         {
-            const int bottomLeft = 0;
-            const int left = 1;
-            const int topLeft = 2;
-            const int top = 3;
-            const int topRight = 4;
-            const int right = 5;
-            const int bottomRight = 6;
-            const int bottom = 7;
+            bool isOverSizedStraight = _axisLength > 1f;
+            bool isOverSeizedDiagonal = _diagonalLength > Constants.SqrtOf2;
+            if (isOverSizedStraight)
+            {
+                if (isOverSeizedDiagonal)
+                {
+                    // generate full quad
+                }
+                else
+                {
+                    // generate over-sized straight
+                }
+            }
+            else
+            {
+                if (isOverSeizedDiagonal)
+                {
+                    // generate over-sized diagonal
+                }
+                else
+                {
+                      Simple.Generate(this);
+                }
+            }
 
-            float axisDistance = _axisLength * 0.5f;
-            float diagonalPerAxisDistance = _diagonalLength * 0.5f * Constants.OneBySqrtOf2;
-
-            SetVertex(bottomLeft, -diagonalPerAxisDistance, -diagonalPerAxisDistance);
-            SetVertex(left, -axisDistance, 0f);
-            SetVertex(topLeft, -diagonalPerAxisDistance, diagonalPerAxisDistance);
-            SetVertex(top, 0f, axisDistance);
-            SetVertex(topRight, diagonalPerAxisDistance, diagonalPerAxisDistance);
-            SetVertex(right, axisDistance, 0f);
-            SetVertex(bottomRight, diagonalPerAxisDistance, -diagonalPerAxisDistance);
-            SetVertex(bottom, 0f, -axisDistance);
-
-            int i = 0;
-
-            // inner top left
-            Indices[i++] = bottomLeft;
-            Indices[i++] = topLeft;
-            Indices[i++] = topRight;
-
-            // left
-            Indices[i++] = bottomLeft;
-            Indices[i++] = left;
-            Indices[i++] = topLeft;
-
-            // top
-            Indices[i++] = topLeft;
-            Indices[i++] = top;
-            Indices[i++] = topRight;
-
-            // inner bottom right
-            Indices[i++] = topRight;
-            Indices[i++] = bottomRight;
-            Indices[i++] = bottomLeft;
-
-            // right
-            Indices[i++] = topRight;
-            Indices[i++] = right;
-            Indices[i++] = bottomRight;
-
-            // bottom
-            Indices[i++] = bottomRight;
-            Indices[i++] = bottom;
-            Indices[i++] = bottomLeft;
-
-            Mesh.SetVertices(Vertices, 0, SimpleOctagonVertexCount);
-            Mesh.SetIndices(Indices, 0, SimpleOctagonIndexCount, MeshTopology.Triangles, 0);
+            Mesh.SetVertices(Vertices, 0, Simple.VertexCount);
+            Mesh.SetIndices(Indices, 0, Simple.IndexCount, MeshTopology.Triangles, 0);
             Mesh.Optimize();
             Mesh.UploadMeshData(true);
+        }
+
+        private static class Simple
+        {
+            public const int VertexCount = 8;
+            public const int IndexCount = 6 * 3;
+            private const int BottomLeft = 0;
+            private const int Left = 1;
+            private const int TopLeft = 2;
+            private const int Top = 3;
+            private const int TopRight = 4;
+            private const int Right = 5;
+            private const int BottomRight = 6;
+            private const int Bottom = 7;
+
+            public static void Generate(OctagonMesh target)
+            {
+                float axisDistance = target._axisLength * 0.5f;
+                float diagonalPerAxisDistance = target._diagonalLength * 0.5f * Constants.OneBySqrtOf2;
+                
+                SetVertex(BottomLeft, -diagonalPerAxisDistance, -diagonalPerAxisDistance);
+                SetVertex(Left, -axisDistance, 0f);
+                SetVertex(TopLeft, -diagonalPerAxisDistance, diagonalPerAxisDistance);
+                SetVertex(Top, 0f, axisDistance);
+                SetVertex(TopRight, diagonalPerAxisDistance, diagonalPerAxisDistance);
+                SetVertex(Right, axisDistance, 0f);
+                SetVertex(BottomRight, diagonalPerAxisDistance, -diagonalPerAxisDistance);
+                SetVertex(Bottom, 0f, -axisDistance);
+
+                int i = 0;
+
+                // inner top left
+                Indices[i++] = BottomLeft;
+                Indices[i++] = TopLeft;
+                Indices[i++] = TopRight;
+
+                // left
+                Indices[i++] = BottomLeft;
+                Indices[i++] = Left;
+                Indices[i++] = TopLeft;
+
+                // top
+                Indices[i++] = TopLeft;
+                Indices[i++] = Top;
+                Indices[i++] = TopRight;
+
+                // inner bottom right
+                Indices[i++] = TopRight;
+                Indices[i++] = BottomRight;
+                Indices[i++] = BottomLeft;
+
+                // right
+                Indices[i++] = TopRight;
+                Indices[i++] = Right;
+                Indices[i++] = BottomRight;
+
+                // bottom
+                Indices[i++] = BottomRight;
+                Indices[i++] = Bottom;
+                Indices[i++] = BottomLeft;
+            }
         }
     }
 }
