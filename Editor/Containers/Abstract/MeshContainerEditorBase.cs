9c768508b38680976544e08524d85c664e0a82d3
misbat <misbat.dev@gmail.com>
2024-08-28 23:22:21 +0200

    fix undo/redo for unselected objects in Unity 2022+

--- Editor/Containers/Abstract/MeshContainerEditorBase.cs
+++ Editor/Containers/Abstract/MeshContainerEditorBase.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
@@ -8,17 +10,58 @@ namespace MichisMeshMakers.Editor.Containers.Abstract
     [CustomEditor(typeof(MeshContainer))]
     public abstract class MeshContainerEditorBase : UnityEditor.Editor
     {
+        // stack of undo records that can be undone
+        private static readonly Stack<UndoRecord> UndoStack = new();
+
+        // inverted stack of undo records that can be redone
+        private static readonly Stack<UndoRecord> RedoStack = new();
+
         private SerializedProperty _meshProperty;
         private SerializedProperty _statisticsProperty;
         private MeshContainer[] _targetMeshContainers;
+        private Mesh[] _targetMeshes;
+
+        static MeshContainerEditorBase()
+        {
+            Undo.undoRedoEvent += OnUndoRedo;
+        }
 
         protected virtual void OnEnable()
         {
             _targetMeshContainers = new MeshContainer[targets.Length];
-            for (int i = 0; i < targets.Length; i++) _targetMeshContainers[i] = (MeshContainer)targets[i];
+            for (int i = 0; i < targets.Length; i++)
+            {
+                _targetMeshContainers[i] = (MeshContainer)targets[i];
+            }
 
             _meshProperty = serializedObject.FindProperty(MeshContainer.MeshFieldName);
             _statisticsProperty = serializedObject.FindProperty(MeshContainer.StatisticsFieldName);
+        }
+
+        private static void OnUndoRedo(in UndoRedoInfo undo)
+        {
+            int undoGroup = undo.undoGroup;
+
+            if (!undo.isRedo) // undo
+            {
+                if (UndoStack.Any() && UndoStack.Peek().Group == undoGroup)
+                {
+                    UndoRecord undoRecord = UndoStack.Pop();
+                    undoRecord.Apply();
+                    RedoStack.Push(undoRecord);
+                    Debug.Log($"Moved undo record {undoRecord.Group} from undo to redo stack");
+                }
+            }
+            else // redo
+            {
+                if (RedoStack.Any() && RedoStack.Peek().Group == undoGroup)
+                {
+                    UndoRecord redoRecord = RedoStack.Pop();
+                    redoRecord.Apply();
+                    UndoStack.Push(redoRecord);
+                    Debug.Log($"Moved undo record {redoRecord.Group} from redo to undo stack");
+                }
+            }
         }
 
         public sealed override void OnInspectorGUI()
@@ -71,10 +114,49 @@ namespace MichisMeshMakers.Editor.Containers.Abstract
 
         protected void Apply()
         {
+            RedoStack.Clear();
+
+            int undoGroup = Undo.GetCurrentGroup();
+
+            string targetsString = string.Join(", ", _targetMeshContainers.Select(tmc => tmc.name));
+            string undoOperationName = $"Apply Container to Mesh in {targetsString}";
+            Undo.RegisterCompleteObjectUndo(_targetMeshContainers.Select(tmc => tmc.Mesh as Object).ToArray(), undoOperationName);
             foreach (MeshContainer targetMeshContainer in _targetMeshContainers)
             {
-                Undo.RecordObject(targetMeshContainer.Mesh, "Apply Mesh Container");
                 targetMeshContainer.Apply();
+            }
+
+            Undo.CollapseUndoOperations(undoGroup);
+
+            UndoStack.Push(new UndoRecord(undoGroup, _targetMeshContainers));
+
+            Debug.Log("Recorded Undo: " + undoGroup);
+        }
+
+        private class UndoRecord
+        {
+            private readonly MeshContainer[] _meshContainers;
+
+            // ReSharper disable once ParameterTypeCanBeEnumerable.Local
+            public UndoRecord(int group, MeshContainer[] meshContainers)
+            {
+                Group = group;
+                _meshContainers = meshContainers.ToArray();
+            }
+
+            public int Group { get; }
+
+            public void Apply()
+            {
+                Debug.Log($"Applying Undo: {Group}");
+                foreach (MeshContainer meshContainer in _meshContainers)
+                {
+                    if (meshContainer != null)
+                    {
+                        meshContainer.Apply();
+                        EditorUtility.SetDirty(meshContainer.Mesh);
+                    }
+                }
             }
         }
     }
